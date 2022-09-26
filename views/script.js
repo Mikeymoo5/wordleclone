6931f985e2b4183d99cd2c719465c125cf2d576b
Mikeymoo5 <moodids@outlook.com>
2022-09-26 21:53:31 +0000

    Basic webpage and server setup

---
+++ views/script.js
@@ -0,0 +1,5 @@
+$.get('messages.txt', function(txt) {
+    var lines = txt.responseText.split("\n");
+    var randLineNum = Math.floor(Math.random() * lines.length);
+    return lines[randLineNum]; // random line from the text file
+    });
